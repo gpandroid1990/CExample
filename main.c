bf99731ef2688eb034bdcf1f937eafd0725f98bd
gpandroid1990 <tibbytang@gmail.com>
2024-10-14 11:25:07 +0800

    v1.0.0 first commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "rock-paper-sclssors/play_game.h"
+
+// 定义方法1
+void test1() {
+    printf("test1 method \n");
+}
+
+// 定义方法2
+void test2() {
+    printf("test2 method \n");
+}
+
+// main 函数，程序入口
+int main(void) {
+//    printf("Hello, World!\n");
+    // 调用方法1
+//    test1();
+    // 调用方法2
+//    test2();
+    testGame();
+    return 0;
+}
